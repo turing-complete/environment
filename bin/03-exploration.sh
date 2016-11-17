1ea39ee88108cdac98886c31596ee131351c4f9f
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-11-17 13:05:58 +0100

    Update the path to the example

--- bin/03-exploration.sh
+++ bin/03-exploration.sh
@@ -4,7 +4,7 @@ set -e
 
 CODE_ROOT="${HOME}/code"
 
-go get -d github.com/turing-complete/example/src
+go get -d github.com/turing-complete/application/src
 ln -s "${GOPATH}/src/github.com/ready-steady" "${CODE_ROOT}/github.com/ready-steady"
 ln -s "${GOPATH}/src/github.com/turing-complete" "${CODE_ROOT}/github.com/turing-complete"
 make -C "${GOPATH}/src/github.com/ready-steady/hdf5" install
